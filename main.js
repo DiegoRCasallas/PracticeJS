8ce8138a449d30b4574c3e7739640b9b0a3140b5
DiegoCasallas <master.drcd@gmail.com>
2022-08-19 16:06:56 -0500

    add components mobileMenu,desktopMenu,manuShopingCard with intectactivity by buttons

--- main.js
+++ main.js
@@ -1,12 +1,75 @@
+/*SELECTORES CLASES HTML */
 /*Traemos el selector del elemento que desplegara nuestro componente en html*/
-const menuEmail= document.querySelector('.navbar-email');
+const menuEmail = document.querySelector('.navbar-email');
 /*traemos el elemnto al cual le quitaremos clases o agregaremos*/
-const desktopMenu=document.querySelector('.desktop-menu');
+const desktopMenu = document.querySelector('.desktop-menu');
+/*Selector elemento navbar*/
+/* mobileMenu*/
+const menuBurgerIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+/*shopingCard*/
+const menuShopingCard = document.querySelector('.shopping-card');
+const iconCard = document.querySelector('.navbar-shoping-cart');
 
-menuEmail.addEventListener('click',toggleDesktopMenu);
 
-function toggleDesktopMenu(){
+
+
+/*ESCUCAMOS EVENTOSde los selectores*/
+menuEmail.addEventListener('click', toggleDesktopMenu);
+/*evento click a menuBurgerIcon que ejecutara la funcion del parámetro*/
+menuBurgerIcon.addEventListener('click', toggleMobileMenu);
+/**/
+iconCard.addEventListener('click', toggleShopingCardMenu);
+
+
+
+
+/*FUNTIONS*/
+
+/**
+ * When the user clicks on the hamburger menu, toggle the inactive class on the desktop menu.
+ */
+function toggleDesktopMenu() {
+    const isCloseShopingCardMenu = menuShopingCard.classList.contains("inactive");
+    if (!isCloseShopingCardMenu) {
+        menuShopingCard.classList.add("inactive");
+    }
     desktopMenu.classList.toggle('inactive');
 }
+/**
+ * If the shopping card menu is closed, toggle the mobile menu. Otherwise, toggle the shopping card
+ * menu and then toggle the mobile menu.
+ */
+function toggleMobileMenu() {
+    const isCloseShopingCardMenu = menuShopingCard.classList.contains('inactive');
+    if (isCloseShopingCardMenu==false) {
+        menuShopingCard.classList.add("inactive");
+    }
+    mobileMenu.classList.toggle("inactive");
+    
+}
+function toggleShopingCardMenu() {
+    const isCloseMobileMenu = mobileMenu.classList.contains('inactive');
+    const isCloseDesktopMenu = desktopMenu.classList.contains('inactive');
 
+    if (!isCloseMobileMenu) {
+        mobileMenu.classList.add("inactive");
+    } 
+    if (!isCloseDesktopMenu) {
+        desktopMenu.classList.add("inactive");
+    } 
+    menuShopingCard.classList.toggle("inactive");
+}
+// function showUltimateMenu(){
+//     const isCloseMobileMenu=mobileMenu.classList.contains('inactive');
+//     const isCloseShopingCardMenu=menuShopingCard.classList.contains('inactive');
+//     console.log("mobile close="+isCloseMobileMenu);
+//     console.log("shoping close="+isCloseShopingCardMenu);
+//     if(isCloseMobileMenu){
+
+
+//     }
+// }  
 console.log('ome gonorrea');
+
+
